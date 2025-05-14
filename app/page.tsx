ab8ab8596d823410d0adebcb9eaaeecdf3dc8b6a
FabianMF1 <Femendoza@uc.cl>
2025-05-14 17:57:28 -0400

    feat: show user courses page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import FeaturedCars from "@/components/FeaturedCars";
 import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
 import MarketTrends from "@/components/MarketTrends";
@@ -7,7 +6,6 @@ export default function HomePage() {
   return (
     <div className="space-y-16">
       <HeroSection />
-      <FeaturedCars />
       <MarketTrends />
       <Footer />
     </div>
